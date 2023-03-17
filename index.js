7981401af4bed918fffe95080496f27791275d7f
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-03-17 12:39:11 -0400

    complete

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json();
+  })
+  .then((json) =>renderBooks(json));
   
+
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
